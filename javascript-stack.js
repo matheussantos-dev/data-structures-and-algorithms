9f7b20b48f1802873fd9fc5a12baccc8dc617899
matheussantos-dev <112334581+matheussantos-dev@users.noreply.github.com>
2023-03-27 18:34:28 -0300

    A simple implementation of a stack in JavaScript

---
+++ javascript-stack.js
@@ -0,0 +1,35 @@
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(item) {
+    this.items.push(item);
+  }
+
+  pop() {
+    if (this.items.length === 0) {
+      return null;
+    }
+    return this.items.pop();
+  }
+
+  peek() {
+    if (this.items.length === 0) {
+      return null;
+    }
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  clear() {
+    this.items = [];
+  }
+
+  size() {
+    return this.items.length;
+  }
+}
